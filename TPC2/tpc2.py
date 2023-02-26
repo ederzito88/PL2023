ae554093e0b9fe648d02005d4628be4686430a10
ederzito88 <tiagoluis.sporting@sapo.pt>
2023-02-26 17:01:56 +0000

    TPC2 feitinho

--- TPC2/tpc2.py
+++ TPC2/tpc2.py
@@ -18,20 +18,23 @@ for line in fileinput.input():
             ints_list = []
             print(soma)
         elif (c.isdigit()):
+            ops_list = []
             ints_list.append(int(c))
         elif(c.isalpha()):
             soma += list_to_int(ints_list)*on
             ints_list = []
-            print("not")
-            if (c in ['o','f','n']): 
+            #print("not")
+            if (c.lower() in ['o','f','n']): 
                 ops_list.append(c)
-                if (''.join(ops_list)) == 'off':
+                #print(''.join(ops_list).lower())
+                if (''.join(ops_list).lower()) == 'off':
                     on = 0
                     print('desliguei')
                     ops_list = []
-                if (''.join(ops_list)) == 'on':
+                if (''.join(ops_list).lower()) == 'on':
                     on = 1
                     print('liguei')
                     ops_list = []
+    if (''.join(ops_list).lower()) not in ['off','on','o', 'of']:  ops_list = []
 
     

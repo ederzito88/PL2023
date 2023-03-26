61f177b91b2146c8810d4823d1bf17322becb9e2
ederzito88 <tiagoluis.sporting@sapo.pt>
2023-03-26 16:40:22 +0100

    TPC3 done

--- TPC3/tpc3D.py
+++ TPC3/tpc3D.py
@@ -1,19 +1,27 @@
+import json
 import re
 
-import re
+first_20_lines = []
+nome_campos = ['id','data','nome','pai','mãe','observações']
 
-f = open('processos.txt','r')
-lines = f.readlines()
-anos = dict()
-
-for line in lines:
-    ano = re.search(r'(\d{4})', line)
-    #print(re.search(r'(\d{4})', line)).group(0)
-    if ano:
-            if ano.group(1) in anos:
-                anos[ano.group(1)]+=1
-            else:
-                anos[ano.group(1)] = 1
-
-print(anos)
+read_file = open("processos.txt", "r")
+for line in read_file:
+    r = re.match(r"\d+::(?P<ano>\d+)-\d{2}-\d{2}::([A-Z][a-z]*) ([A-Z][a-z]* ){0,}([A-Z][a-z]*)", line)
+    if r is not None:
+        campos = re.split("::", line)
+        line_to_json = dict()
+        for i in range(6):
+            line_to_json[nome_campos[i]] = campos[i]
+        first_20_lines.append(line_to_json)
     
+    if len(first_20_lines) >= 20:
+        break
+
+read_file.close()
+    
+dict_processos = dict()
+dict_processos['processos'] = first_20_lines
+
+write_file = open("20proccs.json", "w")
+json.dump(dict_processos, write_file, ensure_ascii=False, indent=2)    
+write_file.close()
